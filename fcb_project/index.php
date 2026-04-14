d95f2fa5ffaa1e25acbe00f2cc99f6e384acc26d
joravv <joravulaa@gmail.com>
2026-04-14 20:37:44 +0200

    fcb

---
+++ fcb_project/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Employe of fcb</title>
+</head>
+<body>
+    <div class="nav bar">
+        <h2> Fcb Employe </h2>
+</div>
+<div class="container">
+    <div class="sidebar">
+        <ul>
+            <li>Dashboard</li>
+            <li>players</li>
+            <li>traning</li>
+            <li>matches</li>
+            <li>messages</li>
+</ul>
+</div>
+<div class="Main">
+    <h2>Welcome to dashboard</h2>
+    <div class="cards">
+        <div class="card"> players:25</div>
+  <div class="card"> matches:3</div>
+    <div class="card"> injuries:2</div>
+
+</div>
+</div>
+
+</body>
+</html>
