2a0134a6cbcd4b63c0219e7eca2e82ca770066cf
joravv <joravulaa@gmail.com>
2026-04-16 20:42:22 +0200

    fcb2

--- fcb_project/index.php
+++ fcb_project/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Employe of fcb</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="nav bar">
@@ -28,6 +29,5 @@
 
 </div>
 </div>
-
 </body>
 </html>
